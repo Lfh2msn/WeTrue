1fff515d9d841bd75b150b4e0808515bd210c326
tsl666 <p_liuyy@landicorp.com>
2021-03-27 21:33:43 +0800

    初始化

---
+++ h5/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="index">
+		这是wetrue
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.index{
+		
+	}
+</style>
